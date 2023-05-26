2e3aadddd25733fc50597f7b876ee633e4a86abc
alex mercer <134717569+amerc011@users.noreply.github.com>
2023-05-26 18:39:52 +0330

    Create JS.js

---
+++ JS.js
@@ -0,0 +1,15 @@
+let topics = new Map {
+  runtime environment
+call stack and heap
+callback queue
+microtasks queue
+event loop
+non-blocking concorrency
+async operation
+execution contenxt
+variable environment
+first class function , high order function
+scope and scope chain
+prototype and prototypal inheritance
+and many more
+}
